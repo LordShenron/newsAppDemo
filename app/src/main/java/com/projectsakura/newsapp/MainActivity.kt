3cd27a846de15e426d14ed57b7472668a218eaec
LordShenron <shen.priyanshu@gmail.com>
2024-04-12 11:37:32 +0530

    newsdemo: remove some redundant imports and code improvements
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- app/src/main/java/com/projectsakura/newsapp/MainActivity.kt
+++ app/src/main/java/com/projectsakura/newsapp/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.projectsakura.newsapp
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
--- app/src/main/java/com/projectsakura/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/projectsakura/newsapp/NewsAdapter.kt
@@ -3,19 +3,11 @@ package com.projectsakura.newsapp
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.google.android.material.textview.MaterialTextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class NewsAdapter(val context: Context) : RecyclerView.Adapter<NewsViewHolder>() {
 
@@ -42,14 +34,10 @@ class NewsAdapter(val context: Context) : RecyclerView.Adapter<NewsViewHolder>()
         Log.d("NewsAdapter", "News content at position $position: ${news.content}")
 
         // Null check before setting title
-        holder.titleTextView.text = news.title?.let {
-            it.trim() // Optional transformation (e.g., trimming)
-        } ?: "No Title Available"
+        holder.titleTextView.text = news.title.trim() ?: "No Title Available"
 
         // Null check before setting title
-        holder.description.text = news.description?.let {
-            it.trim() // Optional transformation (e.g., trimming)
-        } ?: "No Title Available"
+        holder.description.text = news.description.trim() ?: "No Title Available"
 
         // Use Glide to load image if available
         if (news.urlToImage.isNotEmpty()) {
@@ -62,20 +50,13 @@ class NewsAdapter(val context: Context) : RecyclerView.Adapter<NewsViewHolder>()
         // Click listener for the entire item layout
         holder.itemView.setOnClickListener {
             val url = news.url
-
-            if (url != null) {
-                val content = news.content // Retrieve the content from the News object
-
-                // Launch the NewsDetailActivity with the content passed as an extra
-                val intent = Intent(context, NewsDetailActivity::class.java)
-                intent.putExtra("newsUrl", url)
-                intent.putExtra("newsTitle", news.title)
-                intent.putExtra("newsContent", content) // Pass the content data
-                context.startActivity(intent)
-            } else {
-                // Handle case where url is null (e.g., display a toast message)
-                Toast.makeText(context, "Article URL unavailable", Toast.LENGTH_SHORT).show()
-            }
+            val content = news.content // Retrieve the content from the News object
+            // Launch the NewsDetailActivity with the content passed as an extra
+            val intent = Intent(context, NewsDetailActivity::class.java)
+            intent.putExtra("newsUrl", url)
+            intent.putExtra("newsTitle", news.title)
+            intent.putExtra("newsContent", content) // Pass the content data
+            context.startActivity(intent)
         }
     }
 
